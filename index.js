1481edef49912ad3c72490110bb1d609be04c011
ycswaves <ycswaves@gmail.com>
2022-08-12 11:21:35 +0800

    support context

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const core = require('@actions/core');
-const { context } = require('@actions/github');
-const axios = require('axios');
+const core = require("@actions/core");
+const { context } = require("@actions/github");
+const axios = require("axios");
 
 // Trigger the PagerDuty webhook with a given alert
 async function sendAlert(alert) {
   try {
-    const response = await axios.post('https://events.pagerduty.com/v2/enqueue', alert);
+    const response = await axios.post(
+      "https://events.pagerduty.com/v2/enqueue",
+      alert
+    );
 
     if (response.status === 202) {
-      console.log(`Successfully sent PagerDuty alert. Response: ${JSON.stringify(response.data)}`);
+      console.log(
+        `Successfully sent PagerDuty alert. Response: ${JSON.stringify(
+          response.data
+        )}`
+      );
     } else {
       core.setFailed(
-        `PagerDuty API returned status code ${response.status} - ${JSON.stringify(response.data)}`
+        `PagerDuty API returned status code ${response.status
+        } - ${JSON.stringify(response.data)}`
       );
     }
   } catch (error) {
@@ -21,27 +29,33 @@ async function sendAlert(alert) {
 
 // Run the action
 try {
-  const integrationKey = core.getInput('pagerduty-integration-key');
+  const integrationKey = core.getInput("pagerduty-integration-key");
 
   let alert = {
     payload: {
       summary: `${context.repo.repo}: Error in "${context.workflow}" run by @${context.actor}`,
       timestamp: new Date().toISOString(),
-      source: 'GitHub Actions',
-      severity: 'critical',
+      source: "GitHub Actions",
+      severity: "critical",
       custom_details: {
         run_details: `https://github.com/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}`,
         related_commits: context.payload.commits
-          ? context.payload.commits.map((commit) => `${commit.message}: ${commit.url}`).join(', ')
-          : 'No related commits',
+          ? context.payload.commits
+            .map((commit) => `${commit.message}: ${commit.url}`)
+            .join(", ")
+          : "No related commits",
       },
     },
     routing_key: integrationKey,
-    event_action: 'trigger',
+    event_action: "trigger",
   };
-  const dedupKey = core.getInput('pagerduty-dedup-key');
-  if (dedupKey != '') {
+  const dedupKey = core.getInput("pagerduty-dedup-key");
+  if (dedupKey != "") {
     alert.dedup_key = dedupKey;
+  }
+  const context = core.getInput("pagerduty-context");
+  if (Array.isArray(context) && context.length > 0) {
+    alert.payload.context = context;
   }
   sendAlert(alert);
 } catch (error) {
